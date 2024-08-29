b791328ff605396cc1e02f0cc9a06efec80cf60a
aryadwiputra <aryadptr.developer@gmail.com>
2024-08-29 14:12:40 +0700

    feat: add store feature

---
+++ app/Filament/Resources/StoreResource.php
@@ -0,0 +1,89 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\StoreResource\Pages;
+use App\Filament\Resources\StoreResource\RelationManagers;
+use App\Models\Store;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class StoreResource extends Resource
+{
+    protected static ?string $model = Store::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-home';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                // Name
+                Forms\Components\TextInput::make('name')
+                    ->required()
+                    ->maxLength(255),
+                // Address
+                Forms\Components\TextArea::make('address')
+                    ->required()
+                    ->maxLength(255),
+                // Thumbnail
+                Forms\Components\FileUpload::make('thumbnail')
+                    ->required()->image(),
+                // Is Open
+                Forms\Components\Select::make('is_open')
+                    ->required()
+                    ->options([
+                        true => 'Open',
+                        false => 'Closed',
+                    ])
+                    ->default('open'),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\TextColumn::make('name')->sortable()->searchable(),
+                Tables\Columns\TextColumn::make('address')->sortable()->searchable(),
+                Tables\Columns\ImageColumn::make('thumbnail'),
+                Tables\Columns\BooleanColumn::make('is_open')
+                    ->trueIcon('heroicon-s-check-circle')
+                    ->falseIcon('heroicon-s-x-circle')
+                    ->trueColor('success')
+                    ->falseColor('danger'),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListStores::route('/'),
+            'create' => Pages\CreateStore::route('/create'),
+            'edit' => Pages\EditStore::route('/{record}/edit'),
+        ];
+    }
+}
