d29957dceab55db92ce1d1ddf8f8119a4b5d452b
aryadwiputra <aryadptr.developer@gmail.com>
2024-08-29 13:38:19 +0700

    feat: update model file with relations

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -2,10 +2,53 @@
 
 namespace App\Models;
 
+use App\Casts\MoneyCast;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Support\Str;
 
 class Product extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = [
+        'category_id',
+        'brand_id',
+        'name',
+        'slug',
+        'thumbnail',
+        'about',
+        'price',
+    ];
+
+    public function setNameAttribute($value)
+    {
+        $this->attributes['name'] = $value;
+        $this->attributes['slug'] = Str::slug($value);
+    }
+
+    protected $casts = [
+        'price' => MoneyCast::class,
+        'started_at' => 'date',
+        'ended_at' => 'date',
+    ];
+
+
+    public function category(): BelongsTo
+    {
+        return $this->belongsTo(Category::class);
+    }
+
+    public function brand(): BelongsTo
+    {
+        return $this->belongsTo(Brand::class);
+    }
+
+    public function photos(): HasMany
+    {
+        return $this->hasMany(ProductPhoto::class);
+    }
 }
--- app/Models/Transaction.php
+++ app/Models/Transaction.php
@@ -2,10 +2,54 @@
 
 namespace App\Models;
 
+use App\Casts\MoneyCast;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Transaction extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = [
+        'product_id',
+        'store_id',
+        'trx_id',
+        'name',
+        'phone_number',
+        'address',
+        'total_amount',
+        'is_paid',
+        'proof',
+        'duration',
+        'started_at',
+        'ended_at',
+        'delivery_type',
+    ];
+
+    protected $casts = [
+        'total_amount' => MoneyCast::class,
+        'started_at' => 'date',
+        'ended_at' => 'date',
+    ];
+
+    public function generateUniqueTrxId()
+    {
+        $prefix = 'SEWA-';
+        do {
+            $trx_id = $prefix . mt_rand(1000, 9999);
+        } while (Transaction::where('trx_id', $trx_id)->exists());
+        return $trx_id;
+    }
+
+    public function product(): BelongsTo
+    {
+        return $this->belongsTo(Product::class);
+    }
+
+    public function store(): BelongsTo
+    {
+        return $this->belongsTo(Store::class);
+    }
 }
